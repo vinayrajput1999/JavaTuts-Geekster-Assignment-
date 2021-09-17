fcaa39b2ffcfe152d7599f2c0a3c7d4c3aefe05d
vinay <rajvinay834@gmail.com>
2021-09-17 13:36:08 +0530

    java main course

---
+++ precourse java/CountNum.java
@@ -0,0 +1,9 @@
+
+public class CountNum {
+	public static void main(String[] args) {
+		int[] arr = {1,34,2,4,2,5,3,4,35,6,7,89,7,56,7,4,6,6,53,90};
+		
+		System.out.println("Number in Array  " + arr.length);
+	}
+
+}
---
+++ precourse java/ternary_opr.java
@@ -0,0 +1,15 @@
+
+public class ternary_opr {
+	
+           public static void main(String args[]){
+        	   int x = 78;
+        	   int y = 98;
+        	   int max;
+        	   
+        	   max=(x>y)?x:y; {
+        	   
+        	      System.out.println(max);
+        	   }
+           }
+           
+}
